df41250a34adab3cc353da043a506f274d4fee0b
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-05-12 13:01:57 +0600

    added delete api to delete booked services

--- index.js
+++ index.js
@@ -61,6 +61,11 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/bookings/:id", async (req, res) => {
+      const result = await bookingsCollection.deleteOne({ _id: new ObjectId(req.params.id) });
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
